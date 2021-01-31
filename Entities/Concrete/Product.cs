d466c960bf7f5ec4537c961f5345519a90e9be1d
259emine <72345704+259emine@users.noreply.github.com>
2021-02-01 01:40:56 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity//public yapdığımızda diğer katmanlarda ulaşabilsin
+    {
+        public int ProductId { get; set; } 
+        public int CategoryId{ get; set; } //katogori 
+        public string ProductName { get; set; } //ismi
+        public short UnitsInStock { get; set; } //stok adedi
+        public decimal UnitPrice { get; set; } //para birimi
+    }
+}
