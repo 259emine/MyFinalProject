c1fd72c4545566921f03d049ab52ca94d4a1a72b
259emine <72345704+259emine@users.noreply.github.com>
2021-02-07 16:39:03 +0300

    Ürün+Kategoriler

--- DataAccess/Concrete/EntityFramework/EfproductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfproductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,24 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     //NUGET
-    public class EfproductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfproductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-       
+        public List<ProductDetaiIDto> GetProductDetaiIs()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetaiIDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
