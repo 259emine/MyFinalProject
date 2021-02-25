f7986c27e1050e0318e5717651f71e42d48a6c57
259emine <72345704+259emine@users.noreply.github.com>
2021-02-26 01:19:34 +0300

    Autoface kısmı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,15 +23,13 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-
+         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //busines codes yani iş kodları yazılır
+            //loglama kodları çalışıcak
+            //busines codes: iş kodları yazılır
+            //validation: doğrulama kodu 
 
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Massages.ProductNameInvalid);
-            }
             _productDal.Add(product);
 
             return new SuccessResult(Massages.ProductAdded);
